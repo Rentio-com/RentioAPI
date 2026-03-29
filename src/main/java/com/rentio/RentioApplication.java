0edc10de6bf37b3df298b7ebfd0112702750d407
Jakub Cieślik <kuba8172@gmail.com>
2026-03-29 22:51:00 +0200

    feat: initial commit with deps and db ready to development

---
+++ src/main/java/com/rentio/RentioApplication.java
@@ -0,0 +1,13 @@
+package com.rentio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rentio/RentioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
